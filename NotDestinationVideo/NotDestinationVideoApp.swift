995da9cf4c25b56dfd194a76023c258ca2fdaa9f
Mertcan Buyuksandalyaci <mertcan@phantasy.studio>
2023-11-16 17:50:27 -0800

    Initial Commit

---
+++ NotDestinationVideo/NotDestinationVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotDestinationVideoApp.swift
+//  NotDestinationVideo
+//
+//  Created by Mert on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct NotDestinationVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
